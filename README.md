07ec253254b52b317b74ae4b4e75293b50714d4c
foxegg <123628292@qq.com>
2023-12-26 20:24:14 +0800

    init

--- README.md
+++
@@ -1,8 +0,0 @@
-# llg-micro
-spring cloud、vue3、android、mysql、redis、nacos、xxljob
-
-cd index
-npm install
-npm run format
-npm run dev
-npm run build
--- commit.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-dir
-echo start git operation
-xcopy .\index\dist\ .\ /s /e /y
-git add ./
-git commit -m "post"
-git push
-echo 'git operation finish!'
